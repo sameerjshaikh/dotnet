459f5204175ea8ab9b0ae723276e0fedd48116b7
sameer shaikh <46072865+sameerjshaikh@users.noreply.github.com>
2019-12-28 17:25:18 +0530

    Add files via upload

---
+++ mycode/Hello.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    enum WeekDay { Mon, Tue, Wed, Thu, Fri };
+
+    class Hello
+    {
+
+        static void Main(string[] args)
+        {
+            WeekDay startDay = WeekDay.Mon;
+            WeekDay endDay = WeekDay.Fri;
+            Console.WriteLine("The days of the week we work are:");
+            for (int i = (int)startDay; i <= (int)endDay; i++)
+            {
+
+                WeekDay day = (WeekDay)i;
+                Console.WriteLine(day);
+            }
+        }
+
+        /* static void Main(string[] args)
+         {
+
+             var name = "sameer";
+             var num = 123;
+             var rbit = true;
+
+             Type a1 = name.GetType();
+             Type r1 = rbit.GetType();
+
+             Type n2 = num.GetType();
+             Console.WriteLine("Data Type of Name is "+a1);
+             Console.WriteLine("Data Types of name with Diff =:"+a1.ToString());
+             Console.WriteLine("Data Type of Num is " + n2);
+             Console.WriteLine("Data Types of num with Diff =:" + n2.ToString());
+             Console.WriteLine("Data Types of rbit with Diff =:" + r1.ToString());
+
+         }*/
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ mycode/MyCalc.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+    class Myconsole
+    {
+        public static String GetString(String que)
+        {
+            Console.WriteLine(que);
+            return Console.ReadLine();
+        }
+        public static int GetNumber(String que) => int.Parse(GetString(que));
+        public static Double Getdouble(String que) => int.Parse(GetString(que));
+
+    }
+
+
+
+    class MyCalc
+    {
+
+        static void Main(string[] args)
+        {
+            menuFun();
+            //Console.WriteLine("calsi");
+        }
+
+        private static void menuFun()
+        {
+            String stmt = " press 1 for Add \n press 2 for Sub \n press 3 for Mul \n Press any Key for Div......\n";
+           // int process = 1;
+            do
+            {
+                int choice = Myconsole.GetNumber(stmt);
+
+                processOperation(choice);
+       
+
+
+            } while (true);
+        }
+
+        private static void processOperation(int choice)
+        {
+            double first = Myconsole.Getdouble("Enter First Value");
+            double second = Myconsole.Getdouble("Enter Second Value");
+            
+            switch(choice)
+            {
+                case 1:
+                    addfun(first, second);
+                    break;
+                case 2:
+                    subfun(first, second);
+                    break;
+                case 3:
+                    mulfun(first, second);
+                    break;
+                
+                default:
+                    divfun(first, second);
+                    break;
+                    
+
+            }
+
+
+
+        }
+
+        private static void divfun(double first, double second)
+        {
+            double res = first / second;
+            Console.WriteLine($"Division is : {res}");
+        }
+
+        private static void mulfun(double first, double second)
+        {
+            double res = first * second;
+            Console.WriteLine($"Multiplication is : {res}");
+        }
+
+        private static void subfun(double first, double second)
+        {
+            double res = first - second;
+            Console.WriteLine($"Substration is : {res}");
+        }
+
+        private static void addfun(double first, double second)
+        {
+            double res = first + second;
+            Console.WriteLine($"Addation is : {res}");
+        }
+    }
+}
